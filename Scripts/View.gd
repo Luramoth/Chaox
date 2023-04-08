ebc88efb3bffa60ccbc0a74829f2de83588c04ed
Luramoth <85266594+Luramoth@users.noreply.github.com>
2023-04-08 14:52:07 -0700

    add some comments and enforce camera distance

--- Scripts/View.gd
+++ Scripts/View.gd
@@ -4,18 +4,20 @@ extends Camera3D
 @export var Point:Node3D
 
 const CAMERA_DISTANCE:float = 4.7
-const MARGIN:float = 0.1
 const LERP_SPEED:float = 50
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
+	Point.position.z = CAMERA_DISTANCE
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	Ray.force_raycast_update()
 	
 	if (Ray.is_colliding()):
+		# as complicated as this line looks it's actually very simple
+		# all it does is find where to move the camera to so it isent obscured by obsticles
+		# it also does some fancy lerping so it doesent just JUT forward
 		self.position = self.position.lerp(Vector3(0,0,CAMERA_DISTANCE - Ray.get_collision_point().distance_to(Point.global_position)), delta * LERP_SPEED)
 	else:
 		self.position.z = CAMERA_DISTANCE
