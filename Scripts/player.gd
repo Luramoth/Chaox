9df51a3fce38caeed91c7b43722b35cf3e409992
Luramoth <85266594+Luramoth@users.noreply.github.com>
2023-04-08 18:16:57 -0700

    add in acceleration and deceleration to player movement

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,10 +1,12 @@
 extends CharacterBody3D
 
 
-const SPEED:float = 7.0
+const SPEED:float = 6.0
+const ACCELERATION:float = 5.0
+const DECCELERATION:float = 7.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity:float = ProjectSettings.get_setting("physics/3d/default_gravity")
+var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
 
 func _physics_process(delta):
@@ -16,11 +18,10 @@ func _physics_process(delta):
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	
 	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+		velocity = velocity.lerp(Vector3(direction.x * SPEED, velocity.y, direction.z * SPEED), delta * ACCELERATION)
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+		velocity = velocity.lerp(Vector3(0, velocity.y, 0), delta * DECCELERATION)
 
 	move_and_slide()
